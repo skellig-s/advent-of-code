b43a1b82819ad6637f619da65eb37a6e966d6b0b
aleksandr <skellig-s@yandex.ru>
2022-01-09 21:15:20 +0100

    solution generator

---
+++ generator/generator.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const ejs = require('ejs');
+const path = require('path');
+
+const filename = path.join(__dirname, './template.ejs')
+
+const number = process.argv[2];
+const year = process.argv[3] || 2021;
+const pathToOutput = path.join(__dirname, `../${year}/${number}`);
+
+ejs.renderFile(filename, { data : { number } }, {}, (err, fileContent) => {
+    if (!fs.existsSync(`${pathToOutput}/solution.ts`)) {
+        fs.mkdirSync(pathToOutput, { recursive: true });
+
+        fs.writeFileSync(`${pathToOutput}/solution.ts`, fileContent);
+        fs.writeFileSync(`${pathToOutput}/input`, '');
+
+        console.log('Files are successfully created:\n', `${pathToOutput}/solution.ts`, '\n', `${pathToOutput}/input`);
+    }
+});
+
+
