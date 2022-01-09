652d64a2a448d6c873e8f8ff56dead9a1617bc83
aleksandr <skellig-s@yandex.ru>
2022-01-09 17:04:41 +0100

    task 2021.3

--- 2021/3/solution.ts
+++ 2021/3/solution.ts
@@ -1,6 +1,11 @@
 import { processLineByLine } from '../../tools/tasks';
 import { BaseSolution } from '../../tools/interfaces';
 
+enum Bits {
+    zero = '0',
+    one = '1',
+}
+
 class Solution extends BaseSolution {
     private numbersAppearances = new Array(12).fill(0);
 
@@ -29,20 +34,49 @@ class Solution extends BaseSolution {
             }
         }
 
-        const result = eval(`0b${gammaRate.join('')}`) * eval(`0b${epsilon.join('')}`);
+        const result = Solution.binaryToDecimal(gammaRate.join('')) * Solution.binaryToDecimal(epsilon.join(''));
 
         console.log('part 1 answer:', result);
         return result;
     }
 
     async solveSecondTask(): Promise<number> {
-        const result = 1;
+        const arrayOfBinary: String[] = await processLineByLine();
+
+        let oxygen = Solution.findBinaryByCriteria(arrayOfBinary, true);
+        let scrubber = Solution.findBinaryByCriteria(arrayOfBinary, false);
+
+        const result = Solution.binaryToDecimal(oxygen) * Solution.binaryToDecimal(scrubber);
 
         console.log('part 2 answer:', result);
         return result;
+    }
+
+    private static findBinaryByCriteria(input: String[], criteria: boolean, position = 0): String {
+        if (position >= input[0].length) {
+            throw 'No binary according to criteria!';
+        }
+
+        const mostCommonBit = Solution.getMostCommonBit(input, position);
+        const lookBit: Bits = criteria ? mostCommonBit : (+!(+mostCommonBit)).toString() as Bits;
+
+        const filteredInput = input.filter(binary => binary.charAt(position) === lookBit);
+        return filteredInput.length === 1
+            ? filteredInput[0]
+            : Solution.findBinaryByCriteria(filteredInput, criteria, ++position);
+    }
+
+    private static getMostCommonBit(input: String[], position = 0): Bits {
+        let decision = 1; // If they're equal, choose Bits.one as more common
+        input.forEach((bit) => +bit.charAt(position) > 0 ? decision++ : decision--);
+        return decision > 0 ? Bits.one : Bits.zero;
+    }
+
+    private static binaryToDecimal(bin: String): number {
+        return eval(`0b${bin}`);
     }
 }
 
 const solution = new Solution();
 solution.solveFirstTask();
-// solution.solveSecondTask();
+solution.solveSecondTask();
